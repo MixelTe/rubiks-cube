ac40ef30f73260065395120be3ded64acb1cb2d9
Mike <63655075+MixelTe@users.noreply.github.com>
2020-09-27 22:43:22 +0300

    v0.1

--- ts/app/main.ts
+++
@@ -1,5 +0,0 @@
-import * as Lib from "./littleLib";
-import { RubiksCube } from "./rubiksCube/rubiksCube";
-
-const rubiksCube = new RubiksCube(Lib.get.canvas("canvas"));
-rubiksCube.start();
--- ts/rollup-watch.cmd
+++
@@ -1,1 +0,0 @@
-start rollup -c --watch
